96a72f5214c00ee051705529931f0ef378a9c914
Artyom Bebroy <artyom.vasilenko.99@list.ru>
2025-06-06 22:21:35 +0500

    Update config.py

--- rvc/configs/config.py
+++ rvc/configs/config.py
@@ -1,90 +1,63 @@
-import json
-import logging
-from multiprocessing import cpu_count
-
 import torch
+import json
+import os
 
-logger = logging.getLogger(__name__)
-
-
-version_config_list = [
-    "32000.json",
-    "40000.json",
-    "48000.json",
+version_config_paths = [
+    os.path.join("48000.json"),
+    os.path.join("40000.json"),
+    os.path.join("32000.json"),
 ]
 
 
-def singleton_variable(func):
-    def wrapper(*args, **kwargs):
-        if not wrapper.instance:
-            wrapper.instance = func(*args, **kwargs)
-        return wrapper.instance
+def singleton(cls):
+    instances = {}
 
-    wrapper.instance = None
-    return wrapper
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
 
 
-@singleton_variable
+@singleton
 class Config:
     def __init__(self):
-        self.device = "cuda:0"
-        self.is_half = True
-        self.n_cpu = 0
-        self.gpu_name = None
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.gpu_name = (
+            torch.cuda.get_device_name(int(self.device.split(":")[-1]))
+            if self.device.startswith("cuda")
+            else None
+        )
         self.json_config = self.load_config_json()
         self.gpu_mem = None
-        self.instead = ""
         self.x_pad, self.x_query, self.x_center, self.x_max = self.device_config()
 
     def load_config_json(self):
         configs = {}
-        for config_file in version_config_list:
-            with open(f"rvc/configs/{config_file}", "r") as f:
+        for config_file in version_config_paths:
+            config_path = os.path.join("rvc", "configs", config_file)
+            with open(config_path, "r") as f:
                 configs[config_file] = json.load(f)
         return configs
 
-    def device_config(self) -> tuple:
-        if torch.cuda.is_available():
-            i_device = int(self.device.split(":")[-1])
-            self.gpu_name = torch.cuda.get_device_name(i_device)
-            if (
-                ("16" in self.gpu_name and "V100" not in self.gpu_name.upper())
-                or "P40" in self.gpu_name.upper()
-                or "P10" in self.gpu_name.upper()
-                or "1060" in self.gpu_name
-                or "1070" in self.gpu_name
-                or "1080" in self.gpu_name
-            ):
-                logger.info("Found GPU %s, force to fp32", self.gpu_name)
-                self.is_half = False
-            else:
-                logger.info("Found GPU %s", self.gpu_name)
-            self.gpu_mem = int(torch.cuda.get_device_properties(i_device).total_memory / 1024 / 1024 / 1024 + 0.4)
+    def device_config(self):
+        if self.device.startswith("cuda"):
+            self.set_cuda_config()
         else:
-            logger.info("No supported Nvidia GPU found")
-            self.device = self.instead = "cpu"
-            self.is_half = False
+            self.device = "cpu"
 
-        if self.n_cpu == 0:
-            self.n_cpu = cpu_count()
-
-        if self.is_half:
-            # 6G显存配置
-            x_pad = 3
-            x_query = 10
-            x_center = 60
-            x_max = 65
-        else:
-            # 5G显存配置
-            x_pad = 1
-            x_query = 6
-            x_center = 38
-            x_max = 41
-
+        # Configuration for 6GB GPU memory
+        x_pad, x_query, x_center, x_max = (1, 6, 38, 41)
         if self.gpu_mem is not None and self.gpu_mem <= 4:
-            x_pad = 1
-            x_query = 5
-            x_center = 30
-            x_max = 32
+            # Configuration for 5GB GPU memory
+            x_pad, x_query, x_center, x_max = (1, 5, 30, 32)
 
         return x_pad, x_query, x_center, x_max
+
+    def set_cuda_config(self):
+        i_device = int(self.device.split(":")[-1])
+        self.gpu_name = torch.cuda.get_device_name(i_device)
+        self.gpu_mem = torch.cuda.get_device_properties(i_device).total_memory // (
+            1024**3
+        )
